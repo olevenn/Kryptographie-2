c91349782aab19c4aed9a046a7c2381e12bd63df
oleven <julian.pannen@gmail.com>
2024-06-19 14:38:38 +0200

    Uebungsblatt 1 fertig

--- Uebungsblatt 1/T2/ElGamal.py
+++ Uebungsblatt 1/T2/ElGamal.py
@@ -1,3 +1,5 @@
+from sympy import mod_inverse
+
 p = 31
 alpha = 3
 beta = 6
@@ -13,10 +15,30 @@ delta2 = 15
 
 # Aufgabe a) Sind beide Signaturen gültig ?
 
-s1 = (beta**gamma1) * (gamma1**delta1) % p
+s1 = (beta ** gamma1) * (gamma1 ** delta1) % p
 print(s1)
-print((alpha**x)%p)
+print((alpha ** x) % p)
 
-s2 = (beta**gamma2) * (gamma2**delta2) % p
+s2 = (beta ** gamma2) * (gamma2 ** delta2) % p
 print(s2)
-print((alpha**x)%p)
+print((alpha ** x) % p)
+
+## wir schreiben zwei Schleifen für r und a und testen alle Kombinationen aus damit finden wir die Anzahl der gültigen Signaturen heraus
+count = 0
+for r in range(p):
+    gamma = (alpha ** r) % p
+    for a in range(p-1):
+        try:
+            # Berechnung des modularen Inversen von r modulo 28
+            rinverse = mod_inverse(r, p-1)
+            # Berechnung von delta
+            delta = ((x - (a * gamma)) * rinverse) % p-1
+            # Überprüfung der Gleichung
+            if ((beta ** gamma) * (gamma ** delta)) % p == (alpha ** x) % p:
+                count += 1
+                print(f"gamma: {gamma}, delta: {delta}")
+        except ValueError:
+            # Wenn kein modulares Inverses existiert, einfach überspringen
+            continue
+
+print(count)
